2198977f8bdbcf53cf3ec8ac561680f9a9607c1b
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-03-02 17:47:36 -0300

    inciado novo modulo

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,17 @@
+class Maquin(val marca: String) {
+    fun minhaMarca() {
+        println("Minha marca é $marca")
+    }
+}
+
+class Computador(val marca: String, val nucleos: Int) {
+    fun minhaMarca() {
+        println("Minha marca é $marca")
+    }
+    fun ligar(){}
+    fun processar(){}
+}
+
+fun main() {
+
+}
